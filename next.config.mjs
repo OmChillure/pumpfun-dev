8eb5918726c10e2a561f4a071c925f071557a752
omchillure <omchillure4@gmail.com>
2025-02-10 17:14:42 +0530

    allowed origin

--- next.config.mjs
+++ next.config.mjs
@@ -6,9 +6,9 @@ const nextConfig = {
           source: "/api/:path*",
           headers: [
             {
-              key: "Access-Control-Allow-Origin",
-              value: "*",
-            },
+                key: "Access-Control-Allow-Origin",
+                value: "http://localhost:3000",
+              },
             {
               key: "Access-Control-Allow-Methods",
               value: "GET, POST, PUT, DELETE, OPTIONS",
